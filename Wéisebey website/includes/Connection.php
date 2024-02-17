fcebfd0d7213cb3d7e1839ba079d6a2f4cdfd42f
weise1907 <adelbeyiniz@gmail.com>
2024-02-18 00:43:25 +0300

    x

--- Wéisebey website/includes/Connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $con=mysqli_connect('localhost','synltnxd_root','1234567890Selo','synltnxd_root');
-
-    if(!$con)
-    {
-        die(' Please Check Your Connection'.mysqli_error($con));
-    }
-?>
--- Wéisebey website/synl/js/main.js
+++
@@ -1,6 +0,0 @@
-function parayaÇevirAMK(str) {
-    let genel = Intl.NumberFormat('tr-TR');
-    let docs = document.getElementById("para")
-    str = Number(str)
-    if(docs) docs.innerHTML = `${genel.format(str)} TL`
-}
